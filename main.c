bab8fbeeb0b90e7b5c1fa204425bfa5f52cf028c
Hussein Damire <Hudamire@gmail.com>
2026-01-20 13:04:27 -0500

    simple C program

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(void) {
+ int numbers[] = {6, 7};
+ int size = sizeof(numbers) / sizeof(numbers[0]);
+ int sum = 0;
+
+ for (int i = 0; i < size; i++) {
+       sum += numbers[i];
+    }
+
+    printf("Sum: %d\n", sum);
     return 0;
 }
+
