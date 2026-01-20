353b97a1031e1f77af2957cb8dc85c8b5712f2c0
Hussein Damire <Hudamire@gmail.com>
2026-01-20 13:10:26 -0500

    updated variable naming

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 
 int main(void) {
  int numbers[] = {6, 7};
- int size = sizeof(numbers) / sizeof(numbers[0]);
+ int length = sizeof(numbers) / sizeof(numbers[0]);
  int sum = 0;
 
- for (int i = 0; i < size; i++) {
+ for (int i = 0; i < length; i++) {
        sum += numbers[i];
     }
 
